dadebd5d73f7414f29b8869f8ae22587ba35f067
XYZ <xyz@XYZs-MacBook-Pro.local>
2018-11-13 18:58:17 -0500

    add hit me button and pop up alert

--- BullEye/ViewController.swift
+++ BullEye/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert(){
+        //print("Hello!")
+        let alert = UIAlertController(title: "Hello World!", message: "This is Bull-Eye app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style:.default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+# bull-eye-game-app
